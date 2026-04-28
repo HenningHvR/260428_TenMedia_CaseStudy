2ccc5b650bceecd0b965e9ee642c8594ce8394fa
HenningHvR <hratajzak@gmail.com>
2026-04-28 11:02:12 +0200

    Company.php in CompanyPolicy.php umbenannt

--- 260428_TenMedia_CaseStudy/app/Policies/CompanyPolicy.php
+++ 260428_TenMedia_CaseStudy/app/Policies/CompanyPolicy.php
@@ -6,7 +6,7 @@ use App\Models\Company;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class Company
+class CompanyPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class Company
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Company $company): bool
+    public function view(User $user, CompanyPolicy $company): bool
     {
         return false;
     }
@@ -35,7 +35,7 @@ class Company
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Company $company): bool
+    public function update(User $user, CompanyPolicy $company): bool
     {
         return false;
     }
@@ -43,7 +43,7 @@ class Company
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Company $company): bool
+    public function delete(User $user, CompanyPolicy $company): bool
     {
         return false;
     }
@@ -51,7 +51,7 @@ class Company
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Company $company): bool
+    public function restore(User $user, CompanyPolicy $company): bool
     {
         return false;
     }
@@ -59,7 +59,7 @@ class Company
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Company $company): bool
+    public function forceDelete(User $user, CompanyPolicy $company): bool
     {
         return false;
     }

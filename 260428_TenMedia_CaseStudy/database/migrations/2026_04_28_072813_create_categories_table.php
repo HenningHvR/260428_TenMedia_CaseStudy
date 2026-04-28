bebe7ac8dc7739163011758bc9efc98deea66033
HenningHvR <hratajzak@gmail.com>
2026-04-28 10:35:56 +0200

    Spalten in Category eingefügt.

--- 260428_TenMedia_CaseStudy/database/migrations/2026_04_28_072813_create_categories_table.php
+++ 260428_TenMedia_CaseStudy/database/migrations/2026_04_28_072813_create_categories_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
+            // PK
             $table->id();
+
+            // Tabellen-spezifische Attribute
+            $table->string('ctgry_name')->nullable();
+            $table->text('ctgry_description')->nullable();
+
+            // Laraval-Standard -> gibt 'created_at' und 'updated_at' aus
             $table->timestamps();
         });
     }

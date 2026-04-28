73e18bbffd850009c2cda025883b129e16cf9570
HenningHvR <hratajzak@gmail.com>
2026-04-28 10:00:38 +0200

    Attribute zu JobPosting-Tabelle hinzugefügt
    Nach der Erstellung mit der Vorlage, habe ich nun meine zusätzlichen Attribute und FKs eingefügt

--- 260428_TenMedia_CaseStudy/database/migrations/2026_04_28_073008_create_job_postings_table.php
+++ 260428_TenMedia_CaseStudy/database/migrations/2026_04_28_073008_create_job_postings_table.php
@@ -12,8 +12,28 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('job_postings', function (Blueprint $table) {
+            //PK
             $table->id();
+
+            // Tabellen-spezifische Attribute
+            $table->string('title');
+            $table->text('jp_description')->nullable();
+            $table->string('jp_location')->nullable();
+            $table->string('experience-level')->nullable();
+            $table->string('employment_type')->nullable();
+            $table->decimal('salary', 8, 2)->nullable();
+            $table->boolean('is_active')->default(true);
+
+            // Laraval-Standard -> gibt 'created_at' und 'updated_at' aus
             $table->timestamps();
+            
+            // FK hinzufügen
+            $table->foreignId('company_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('category_id')
+                ->constrained()
+                ->restrictOnDelete();
         });
     }
 
